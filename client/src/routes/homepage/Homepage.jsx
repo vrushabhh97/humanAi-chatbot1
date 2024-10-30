87f55dc54639b7395f0fa704e9015242d06b2cbb
Vrushabh Deogirikar <68142086+vrushabhh97@users.noreply.github.com>
2024-10-30 13:40:31 -0700

    first commit

---
+++ client/src/routes/homepage/Homepage.jsx
@@ -0,0 +1,87 @@
+import { Link } from "react-router-dom";
+import "./homepage.css";
+import { TypeAnimation } from "react-type-animation";
+import { useState } from "react";
+
+const Homepage = () => {
+  const [typingStatus, setTypingStatus] = useState("human1");
+
+  // const test = async () => {
+  //   await fetch("http://localhost:3000/api/test",{
+  //     credentials:"include",
+  //   });
+  // };
+
+  return (
+    <div className="homepage">
+      <img src="/orbital.png" alt="" className="orbital" />
+      <div className="left">
+        <h1>HUMAN AI</h1>
+        <h2>Can you outsmart the personalities?</h2>
+        <h3>
+        Engage in a classic game of trust and betrayal—explore how different personalities react!
+        </h3>
+        <Link to="/dashboard">Get Started</Link>
+      </div>
+      <div className="right">
+        <div className="imgContainer">
+          <div className="bgContainer">
+            <div className="bg"></div>
+          </div>
+          <img src="/bot.png" alt="" className="bot" />
+          <div className="chat">
+            <img
+              src={
+                typingStatus === "human1"
+                  ? "/human1.jpeg"
+                  : typingStatus === "human2"
+                  ? "/human2.jpeg"
+                  : "bot.png"
+              }
+              alt=""
+            />
+            <TypeAnimation
+              sequence={[
+                // Same substring at the start will only be typed out once, initially
+                "Human:We produce food for Mice",
+                2000,
+                () => {
+                  setTypingStatus("bot");
+                },
+                "Bot:We produce food for Hamsters",
+                2000,
+                () => {
+                  setTypingStatus("human2");
+                },
+                "Human2:We produce food for Guinea Pigs",
+                2000,
+                () => {
+                  setTypingStatus("bot");
+                },
+                "Bot:We produce food for Chinchillas",
+                2000,
+                () => {
+                  setTypingStatus("human1");
+                },
+              ]}
+              wrapper="span"
+              repeat={Infinity}
+              cursor={true}
+              omitDeletionAnimation={true}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="terms">
+        <img src="/logo.png" alt="" />
+        <div className="links">
+          <Link to="/">Terms of Service</Link>
+          <span>|</span>
+          <Link to="/">Privacy Policy</Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
